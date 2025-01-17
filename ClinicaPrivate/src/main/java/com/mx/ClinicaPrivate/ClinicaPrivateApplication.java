dda416c315834528f953b8f6abed06958e6beb83
DevCheke <ezequielliconacazares@gmail.com>
2025-01-16 23:00:13 -0600

    Primera prueba

---
+++ ClinicaPrivate/src/main/java/com/mx/ClinicaPrivate/ClinicaPrivateApplication.java
@@ -0,0 +1,13 @@
+package com.mx.ClinicaPrivate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaPrivateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaPrivateApplication.class, args);
+	}
+
+}
---
+++ ClinicaPrivate/src/test/java/com/mx/ClinicaPrivate/ClinicaPrivateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.ClinicaPrivate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaPrivateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
